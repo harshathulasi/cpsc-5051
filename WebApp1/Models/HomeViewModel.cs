ec7514dc0dba0f6283ae4ee1ad1336ba1459ebaf
Thulasi, Harsha <harsha0417@gmail.com>
2019-05-21 22:42:22 -0700

    Added HomeViewModel.cs

---
+++ WebApp1/Models/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class HomeViewModel
+{
+    public int PhoneCount;
+    public int UserCount;
+    public DateTime LastAccessDate;
+}
